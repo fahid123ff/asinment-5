b8ee6897ee33c2035f6b47c5ad2cf0f01ab89dd8
fahid123ff <mdfahidfardos@gmail.com>
2024-04-17 14:10:04 +0600

    start dom added

--- script/index.js
+++ script/index.js
@@ -1,1 +1,21 @@
-console.log( 'fahid');
+
+function handleClick() {
+    const ticketSection = document.getElementById('showTiketSection');
+    ticketSection.classList.remove('hidden');
+}
+
+let count = 0;
+
+// all button
+const allBtn = document.getElementsByClassName('kbd');
+for(const btn of allBtn){
+    btn.addEventListener('click', function(){
+        count = count + 1;
+        document.getElementById('seatCapibality').innerText = count;
+
+        const seatsLeftElement = document.getElementById('seatsLeft');
+        const dueSite =  40 - count;
+         seatsLeftElement.innerText = dueSite;
+         
+    })
+}
