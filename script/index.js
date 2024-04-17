16c9c5c33311a19fea021abfe9d94e9b75f712c9
fahid123ff <mdfahidfardos@gmail.com>
2024-04-17 20:08:01 +0600

    compleate price section

--- script/index.js
+++ script/index.js
@@ -5,17 +5,46 @@ function handleClick() {
 }
 
 let count = 0;
-
+let price = 0;
 // all button
 const allBtn = document.getElementsByClassName('kbd');
 for(const btn of allBtn){
     btn.addEventListener('click', function(){
         count = count + 1;
-        document.getElementById('seatCapibality').innerText = count;
+        if(count < 5){
+            document.getElementById('seatCapibality').innerText = count;
 
-        const seatsLeftElement = document.getElementById('seatsLeft');
-        const dueSite =  40 - count;
-         seatsLeftElement.innerText = dueSite;
+            const seatsLeftElement = document.getElementById('seatsLeft');
+            const dueSite =  40 - count;
+             seatsLeftElement.innerText = dueSite;
+        }else{
+            alert();
+        }
+        
+        
+      
+
+      
+        event.target.style.backgroundColor = "#1DD100";
+        event.target.style.color = "white";
+
+        price = price + 550;
+        
+        
+
+        
+        const sitNumber = event.target.innerText;
+        
+         document.getElementById('sitName').innerText =sitNumber;
+         
+
+      setInnerText('totalPrices',price )
+      setInnerText('grandPrices',price )
          
     })
+}
+
+
+function setInnerText(id, value){
+    document.getElementById(id).innerText = value;
 }
